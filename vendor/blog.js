7abc429696730d2179d75d6e9453d6711a38cd77
zhanzengyu <553838510@qq.com>
2022-10-16 11:31:53 +0800

    slient博客加速，增加 README 和 ignore 文件，删除无用目录

--- vendor/blog.js
+++ vendor/blog.js
@@ -3,44 +3,69 @@
  */
 
 ;(function () {
-
   'use strict'
 
   var pageExt, pageBase
   var sidebarPage, defaultPage
   var mainPage, mainTitle
+  var mainPageId, mainSearch
+
+  function loadSidebar() {
+    load('#sidebar-page', sidebarPage)
+  }
+
+  function loadMain(search, callback) {
+    mainSearch = search
+    var seg = search.slice(1).replace(/&.*$/g, '')
+    // fucking wechat again
+    // like /?graduation-thanks=
+    // or /?graduation-thanks=/ (SublimeServer)
+    seg = seg.replace(/=[/\\]*$/, '')
+    // fucking wechat pending
+    // like /?from=singlemessage&isappinstalled=0
+    if (/=/.test(seg)) seg = null
+    mainPage = resolve(seg || defaultPage)
+    load('#main-page', mainPage, true, callback)
+  }
 
   function load(sel, stack, isMain, callback) {
     if (typeof stack === 'string') {
       if (/\/$/.test(stack)) {
         stack = [stack + 'index', stack + 'README', stack.replace(/\/$/, '')]
       } else {
-        stack = [stack, stack + '/index']
+        stack = [stack, stack + '/index', stack + '/README']
       }
     }
 
-    var page = stack.shift()
+    var pageId = stack.shift()
     isMain = isMain || false
+    if (isMain) mainPageId = pageId
 
-    var url = pageBase + page + pageExt
+    var url = pageBase + pageId + pageExt
     $.ajax({
       url: url,
       error: function (err) {
+        if (isMain && pageId !== mainPageId) return
+
         if (stack.length) {
           return load(sel, stack, isMain, callback)
         }
-        onNotFound(err)
+        if (isMain) onNotFound(err)
       },
       success: function (data) {
+        if (isMain && pageId !== mainPageId) return
+
         render(data, function (err, html) {
+          if (isMain && pageId !== mainPageId) return
+
           if (err) {
             return console.error('render err', err)
           }
           var $el = $(sel)
-          $el.hide().html(html)
+          $el.addClass('contents-preparing').html(html)
           postProcess($el, url)
 
-          $el.show().attr('data-loaded', true)
+          $el.removeClass('contents-preparing').attr('data-loaded', true)
           if (isMain) onMainRendered()
           if (callback) callback()
         })
@@ -49,7 +74,7 @@
   }
 
   function postProcess($el, url) {
-    var dir = url.replace(new RegExp('[^\\/]*$', 'g'), '')
+    var dir = url.replace(/[^\\/]*$/, '')
 
     $el.find('[src]').each(function () {
       var $el = $(this)
@@ -76,7 +101,7 @@
           return old
         }
         var prefixed = resolve(dir + old)
-        var hashRegex = new RegExp('#.*')
+        var hashRegex = /#.*/
         var hash = (function (match) {
           return match && match[0] || ''
         })(old.match(hashRegex))
@@ -91,10 +116,6 @@
               .replace(extRegex, '') +
             hash
           )
-        }
-        if (new RegExp('^\\.\\/').test(old)) {
-          // ./ heading for new tag
-          $el.attr('target', '_blank')
         }
         return prefixed
       })
@@ -118,10 +139,6 @@
     document.title = navTitle
 
     // supports mermaid diagrams
-    // https://mermaid-js.github.io/mermaid/#/usage?id=calling-mermaidinit
-    mermaid.parseError = function (err, hash) {
-      console.error('mermaid.parseError', err, hash)
-    }
     mermaid.init()
 
     comments()
@@ -137,11 +154,14 @@
   }
 
   function slashes(str) {
-    return str.replace(/([.?*+^$!:\[\]\\(){}|-])/g, '\\$1')
+    return str.replace(/([.?*+^$!:[\]\\(){}|-])/g, '\\$1')
   }
 
+  // How to test if a URL string is absolute or relative?
+  // https://stackoverflow.com/questions/10687099/how-to-test-if-a-url-string-is-absolute-or-relative
+  // https://en.wikipedia.org/wiki/Uniform_Resource_Identifier#Syntax
   function isAbsolute(url) {
-    return !url.indexOf('//') || !!~url.indexOf('://')
+    return /^(ftp:|https?:)?\/\//i.test(url) || /^(mailto|tel):/i.test(url)
   }
 
   function resolve(path) {
@@ -185,11 +205,14 @@
     })
   }
 
+  // TODO library extraction: title-favicon & text-favicon & emoji-detect
   // How to detect emoji using javascript
   // https://stackoverflow.com/questions/18862256/how-to-detect-emoji-using-javascript
-  // +modification title-favicon bugfix: recognize emoji `✋🏻`
-  var emojiPrefixRegex = /^(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])+/
-  var anyPrefixRegex = /^(?:(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])+|.)/
+  // +modification title-favicon bugfix: recognize emoji `✋🏻` `💁‍♀️` `👨‍👩‍👧‍👦`
+  var emojiCellRegex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])+/
+  var emojiRegex = new RegExp(emojiCellRegex.toString().replace(/^\/(.+)\/$/, '(?:[\\u200d]*$1)+[\\ufe0f]*'))
+  var emojiPrefixRegex = new RegExp(emojiRegex.toString().replace(/^\/(.+)\/$/, '^$1'))
+  var anyPrefixRegex = new RegExp(emojiPrefixRegex.toString().replace(/^\/\^(.+)\/$/, '^(?:$1|.)'))
 
   // Use emoji as favicon in websites
   // https://stackoverflow.com/questions/59431371/use-emoji-as-favicon-in-websites
@@ -205,17 +228,22 @@
       ctx.fillStyle = isDarkMode ? 'orange' : '#336699' // for text color
       ctx.textAlign = 'center'
       ctx.fillText(s, 16, 24)
-      var link = document.querySelector('link[rel=icon]')
-      var hasLinkAlready = !!link
-      if (!hasLinkAlready) {
-        link = document.createElement('link')
-        link.setAttribute('rel', 'icon')
-      }
-      link.setAttribute('href', canvas.toDataURL())
-      if (!hasLinkAlready) {
-        var parent = document.querySelector('head') || document.documentElement
-        parent.appendChild(link)
-      }
+      var dataUrl = canvas.toDataURL()
+
+      var parent = document.querySelector('head') || document.documentElement
+      var rels = ['icon']
+      rels.forEach(function (key) {
+        var link = document.querySelector('link[rel=' + key + ']')
+        if (link) {
+          link.setAttribute('href', dataUrl)
+        } else {
+          link = document.createElement('link')
+          link.setAttribute('rel', key)
+          link.setAttribute('href', dataUrl)
+          parent.appendChild(link)
+        }
+      })
+
       lastFavicon = s
       return true // success flag
     } catch (err) {
@@ -225,12 +253,25 @@
     }
   }
   darkModeChangeHandlers.push(function () {
-    setFavicon(lastFavicon)
+    if (lastFavicon) setFavicon(lastFavicon)
   })
+
+  function detectShouldApplyFavicon() {
+    var ua = navigator.userAgent
+    var isMobile = /Mobile[/ ]|Android|iPad/.test(ua) // confidence: high
+    var isHuaweiBr = /HuaweiBrowser/.test(ua) // confidence: high
+    var isWechat = /MicroMessenger|Wechat|Weixin/.test(ua) // confidence: high
+    var isQQ = /M?QQBrowser/.test(ua) // confidence: high
+    var isSafari = /Version\/[\d.]+\s+Safari/.test(ua) // confidence: low
+    return !isWechat && !isSafari && (isMobile ? isHuaweiBr || isQQ : true)
+  }
+  var shouldApplyFavicon = detectShouldApplyFavicon()
 
   function autoTitleFavicon(mainTitle, emojiOnly) {
     var regex = emojiOnly ? emojiPrefixRegex : anyPrefixRegex
     var navTitle = mainTitle
+    if (!shouldApplyFavicon) return navTitle
+
     var matched = mainTitle.match(regex)
     if (matched) {
       var prefix = matched[0]
@@ -242,8 +283,59 @@
     return navTitle
   }
 
+  // opt.1 disqus
+  // https://disqus.com/admin/install/platforms/universalcode/
+  var disqusInitiated = false
+  function disqus(shortName, title, id) {
+    /* global DISQUS, disqus_shortname */
+    window.disqus_shortname = shortName
+    window.disqus_title = title
+    window.disqus_identifier = id
+    window.disqus_url = location.href
+    // Using Disqus on AJAX sites
+    // https://help.disqus.com/en/articles/1717163-using-disqus-on-ajax-sites
+    if (disqusInitiated) {
+      if (window.DISQUS) DISQUS.reset({ reload: true })
+      return
+    }
+    disqusInitiated = true
+    $('<div>').attr({ id: 'disqus_thread' }).appendTo('#comment-system')
+    // adding setTimeout to prevent favicon from keeping loading instead of showing
+    // (disqus.com gets blocked when it's in GFW)
+    setTimeout(function () {
+      $('<script>').attr({
+        src: 'https://' + disqus_shortname + '.disqus.com/embed.js',
+        'data-timestamp': +new Date()
+      }).appendTo('body')
+    }, 1000)
+  }
+  // opt.2 cusdis
+  // https://cusdis.com/doc#/advanced/sdk?id=js-sdk
+  // TODO: adapt for SPA
+  function cusdis(host, appId, title, id) {
+    $('#comment-system').empty()
+    $('<div>').attr({
+      id: 'cusdis_thread',
+      'data-host': host,
+      'data-app-id': appId,
+      'data-page-id': id,
+      'data-page-url': location.href,
+      'data-page-title': title
+    }).appendTo('#comment-system')
+    $('<script>').attr({
+      src: 'https://cusdis.com/js/cusdis.umd.js',
+      async: true
+    }).appendTo('head')
+  }
+  // opt.3 giscus
   // https://giscus.app/
+  var giscusInitiated = false
   function giscus(attrs) {
+    if (giscusInitiated) {
+      giscusSendMessage({ setConfig: { term: document.title } })
+      return
+    }
+    giscusInitiated = true
     $('<div>').addClass('giscus').appendTo('#comment-system')
     var dest = {
       src: 'https://giscus.app/client.js',
@@ -262,17 +354,94 @@
     Object.keys(dest).forEach(function (k) { script.setAttribute(k, dest[k]) })
     document.body.appendChild(script)
   }
+  // dynamically setConfig
+  // https://github.com/giscus/giscus/blob/main/ADVANCED-USAGE.md#parent-to-giscus-message-events
+  function giscusSendMessage(message) {
+    var iframe = document.querySelector('iframe.giscus-frame')
+    if (!iframe) return
+    iframe.contentWindow.postMessage({ giscus: message }, 'https://giscus.app')
+  }
+
+  // body.scrollTop vs documentElement.scrollTop vs window.pageYOffset vs window.scrollY
+  // https://stackoverflow.com/questions/19618545/body-scrolltop-vs-documentelement-scrolltop-vs-window-pageyoffset-vs-window-scro
+  function getScrollTop() {
+    return window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0
+  }
+
+  function preferPJAX() {
+    if (!('pushState' in history)) return
+    if ('scrollRestoration' in history) {
+      history.scrollRestoration = 'manual'
+    }
+    window.addEventListener('scroll', _.throttle(function () {
+      var newState = _.assign(history.state || {}, { scrollTop: getScrollTop() })
+      // temporally disable Pace
+      // https://github.com/CodeByZach/pace/blob/5ba323c/pace.js#L14-L40
+      Pace.options.restartOnPushState = false
+      history.replaceState(newState, '', document.URL)
+      Pace.options.restartOnPushState = true
+    }, 500))
+    window.addEventListener('popstate', function (e) {
+      var savedState = e.state || {}
+      var savedScrollTop = savedState.scrollTop || 0
+      loadMain(location.search, function () {
+        window.scrollTo(0, savedScrollTop)
+      })
+      adaptForTripleBackBehavior()
+    })
+    // trying to fix: continuous popstate events may not be fired properly
+    // seems to be a fucking weird feature by some browsers:
+    // back btn being pressed too frequently (triple-click)
+    var popstateDelayTimer
+    function adaptForTripleBackBehavior() {
+      clearTimeout(popstateDelayTimer)
+      popstateDelayTimer = setTimeout(function () {
+        if (location.search !== mainSearch) {
+          console.log('popstate got lost detected location.search=', location.search, 'mainSearch=', mainSearch)
+          loadMain(location.search)
+        }
+      }, 500)
+    }
+    $('body').delegate('[href]', 'click', function (e) {
+      var $a = $(e.target)
+      var url = $a.attr('href') || ''
+      var target = $a.attr('target')
+      var isTargetSelf = [undefined, '_self'].indexOf(target) > -1
+      var isSilentInternal = url === '.' || /^\?/.test(url)
+      var isSameUrl = url === location.search || url === '' || url === '.' && !location.search
+      if (isTargetSelf && isSilentInternal) {
+        e.preventDefault()
+        // explicit call Pace in case of no pushState
+        // Pace.restart: Called automatically whenever pushState or replaceState is called by default.
+        Pace.restart()
+        loadMain(url, function () {
+          window.scrollTo(0, 0)
+          if (!isSameUrl) history.pushState({}, '', url)
+        })
+      }
+    })
+  }
 
   config()
   start()
 
   function render(data, callback) {
-    //// Optional template renderer
+    // -- Optional template renderer
     marked(data, callback)
   }
 
   function comments() {
-    //// Optional comment system
+    // -- Optional comment system
+    // opt.1 disqus (not recommended in China due to the GFW)
+    // var dqsShortName = 'silent-blog'
+    // disqus(dqsShortName, mainTitle, mainPage)
+
+    // opt.2 cusdis
+    // var cdsHost = 'https://cusdis.com'
+    // var cdsAppId = '3ab3a14f-bcb2-4a6f-b984-742a15463f80'
+    // cusdis(cdsHost, cdsAppId, mainTitle, mainPage)
+
+    // opt.3 giscus
     giscus({
       'data-repo': 'zhanzengyu/blog',
       'data-repo-id': 'R_kgDOIK5_vA',
@@ -283,27 +452,25 @@
   }
 
   function shares() {
-    //// Optional share system
+    // -- Optional share system
   }
 
   function start() {
-    var seg = location.search.slice(1).replace(/&.*$/g, '')
-
-    // fucking wechat again
-    // like /?graduation-thanks=
-    // or /?graduation-thanks=/ (SublimeServer)
-    seg = seg.replace(/=[\/\\]*$/, '')
-
-    // fucking wechat pending
-    // like /?from=singlemessage&isappinstalled=0
-    if (/=/.test(seg)) seg = null
-    mainPage = resolve(seg || defaultPage)
-
-    load('#sidebar-page', sidebarPage)
-    load('#main-page', mainPage, true)
+    loadSidebar()
+    loadMain(location.search)
   }
 
   function config() {
+    // Optional: history.pushState API (PJAX) for silent internal page navigation
+    preferPJAX()
+
+    // supports mermaid diagrams
+    mermaid.mermaidAPI.initialize({ startOnLoad: false })
+    // https://mermaid-js.github.io/mermaid/#/usage?id=calling-mermaidinit
+    mermaid.parseError = function (err, hash) {
+      console.error('mermaid.parseError', err, hash)
+    }
+
     var renderer = new marked.Renderer()
 
     // 实现trello的 超链接效果 自动识别github issues
@@ -379,6 +546,6 @@
     pageBase = 'p/'
     // add a trailing slash if it is an index.md of a directory
     sidebarPage = 'sidebar'
-    defaultPage = 'projects'
+    defaultPage = 'posts'
   }
 })()
